98f4f90ce79ba3305809e50e32ea9c94c7860918
vitialell <vlirianoluis@gmail.com>
2023-12-06 15:31:31 -0500

    Message in main Class

--- src/main/java/com/lirianosclening/llc/LlcApplication.java
+++ src/main/java/com/lirianosclening/llc/LlcApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LlcApplication {
 
+	//testing code
+
 	public static void main(String[] args) {
 		SpringApplication.run(LlcApplication.class, args);
 	}
